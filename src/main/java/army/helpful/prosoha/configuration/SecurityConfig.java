059731579dff9b7b7828a8e1cbb57a66c08839df
murat.doner <murat.doner@atg.se>
2019-03-24 19:04:19 +0100

    Keycloak added

---
+++ src/main/java/army/helpful/prosoha/configuration/SecurityConfig.java
@@ -0,0 +1,100 @@
+package army.helpful.prosoha.configuration;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.io.InputStream;
+import java.util.Arrays;
+
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter
+{
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakConfigResolver() {
+
+            private KeycloakDeployment keycloakDeployment;
+
+            @Override
+            public KeycloakDeployment resolve(HttpFacade.Request facade) {
+                if (keycloakDeployment != null) {
+                    return keycloakDeployment;
+                }
+
+                String path = "/keycloak.json";
+                InputStream configInputStream = getClass().getResourceAsStream(path);
+
+                if (configInputStream == null) {
+                    throw new RuntimeException("Could not load Keycloak deployment info: " + path);
+                } else {
+                    keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
+                }
+
+                return keycloakDeployment;
+            }
+        };
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/admin/system/state/*",
+                        "/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll()
+                .antMatchers("/title/content/create").authenticated()
+                .antMatchers("/sso/login*").permitAll()
+                .antMatchers("/*").hasRole("ADMIN")
+                .anyRequest().permitAll();
+        http.csrf().disable();
+
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://www.helpful.army"
+                , "https://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+}
