51833273edafca8b8cac3ad62a3bdcbed6c21965
murat.doner <murat.doner@atg.se>
2019-03-28 22:59:42 +0100

    fix for "first content"

--- src/main/java/army/helpful/prosoha/message/model/Content.java
+++ src/main/java/army/helpful/prosoha/message/model/Content.java
@@ -1,12 +1,14 @@
 package army.helpful.prosoha.message.model;
 
 
-
-
 public class Content extends BasicModel {
 
     private String text;
     private Title title;
+
+
+    private boolean firstContent;
+
     public Content() {
     }
 
@@ -26,4 +28,18 @@ public class Content extends BasicModel {
     public void setTitle(Title title) {
         this.title = title;
     }
+
+    public boolean isFirstContent() {
+        return firstContent;
+    }
+
+    public void setFirstContent(boolean firstContent) {
+        this.firstContent = firstContent;
+    }
+
+
+    public String toString(){
+        return   this.getClass().getSimpleName();
+    }
+
 }
